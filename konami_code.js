d0249d5fa7966ad9217a4ae83230d57419fc8bbb
Mendy <mendybeltcohen@gmail.com>
2018-05-02 15:32:16 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,15 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
   // Write your JavaScript code inside the init() function
-
+  let tracker = [];
+  document.body.addEventListener('keydown', function(e){
+    tracker.push(e.which || e.detail);
+    //if length == code.length
+    if(tracker.length === code.length){
+      //if tracker == code : success
+      alert('great job buddy!');
+    } //else remove first element
+      else{code.shift()}
+  }); 
 }
